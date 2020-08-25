186fe0430f0b25479f1e462ccb25791164c70dbc
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-25 20:44:05 -0300

    comandos

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ client.on("ready", () => {
 
 client.on("message", async message => {
 
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    if(!message.content.startsWith(config.prefix)) return;
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  if(!message.content.startsWith(config.prefix)) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const comando = args.shift().toLowerCase();
@@ -20,9 +20,32 @@ client.on("message", async message => {
   // ping
   if(comando === "ping") {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
   }
-
+  else if(comando === "help"){
+    await message.channel.send("> **Comandos:** grana | empresta | valor | custo | perdao | relaxar | diadasmaes")
+  }
+  else if(comando === "grana") {
+    await message.channel.send("> E quanto isso vai me custar??");
+  }
+  else if(comando === "empresta") {
+    await message.channel.send("> Quer saber o que é mágica? Eu tenho dois empregos, trabalho sete dias por semana e todo dia meu dinheiro desaparece.");
+  }
+  else if(comando === "valor"){
+    await message.channel.send("> Aceita vale-refeição?")
+  }
+  else if(comando === "custo"){
+    await message.channel.send("> Desliga esse relógio da tomada, garoto, você não vê as horas enquanto dorme. São 2 centavos por hora!")
+  }
+  else if(comando === "diadasmaes"){
+    await message.channel.send("> Porque eu deveria comprar algo? É Dia das Mães, e não das Esposas!")
+  }
+  else if(comando === "perdao"){
+    await message.channel.send("> Sabe quantas vezes eu estava certo e tive que pedir perdão? 4.351 vezes!")
+  }
+  else if(comando === "relaxar"){
+    await message.channel.send("> Por que eu vou sair pra relaxar se eu posso relaxar em casa que é grátis?")
+  }
 });
 
 client.login(process.env.TOKEN);

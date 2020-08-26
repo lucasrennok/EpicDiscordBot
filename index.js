247239c76fd5c39ab9b0c8cb76e2d86595731acb
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-25 21:52:59 -0300

    say

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const config = require("./config.json");
 require('dotenv').config();
 
 const client = new Discord.Client(); 
+const falas = [
+  "Se eu não comprar nada, o desconto é maior.",
+  "E quanto isso vai me custar??",
+  "Quer saber o que é mágica? Eu tenho dois empregos, trabalho sete dias por semana e todo dia meu dinheiro desaparece.",
+  "Aceita vale-refeição?",
+  "Desliga esse relógio da tomada, garoto, você não vê as horas enquanto dorme. São 2 centavos por hora!",
+  "Porque eu deveria comprar algo? É Dia das Mães, e não das Esposas!",
+  "Sabe quantas vezes eu estava certo e tive que pedir perdão? 4.351 vezes!",
+  "Por que eu vou sair pra relaxar se eu posso relaxar em casa que é grátis?",
+]
+const allCommands = [
+  "help",
+  "say",
+  "ping"
+]
 
 client.on("ready", () => {
   console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
@@ -15,37 +30,23 @@ client.on("message", async message => {
   if(!message.content.startsWith(config.prefix)) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const comando = args.shift().toLowerCase();
+  const command = args.shift().toLowerCase();
   
-  // ping
-  if(comando === "ping") {
+  // commands here
+  if(command === "ping") {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
   }
-  else if(comando === "help"){
-    await message.channel.send("> **Comandos:** grana | empresta | valor | custo | perdao | relaxar | diadasmaes")
+  else if(command === "help"){
+    await message.channel.send("> **Commands:** " + allCommands)
   }
-  else if(comando === "grana") {
-    await message.channel.send("> E quanto isso vai me custar??");
+  else if(command === "say") {
+    const somethingToSay = falas[Math.floor(Math.random() * falas.length)];
+    await message.channel.send("> "+somethingToSay);
+  }else{
+    await message.channel.send("> Type $$help to get the available commands.")
   }
-  else if(comando === "empresta") {
-    await message.channel.send("> Quer saber o que é mágica? Eu tenho dois empregos, trabalho sete dias por semana e todo dia meu dinheiro desaparece.");
-  }
-  else if(comando === "valor"){
-    await message.channel.send("> Aceita vale-refeição?")
-  }
-  else if(comando === "custo"){
-    await message.channel.send("> Desliga esse relógio da tomada, garoto, você não vê as horas enquanto dorme. São 2 centavos por hora!")
-  }
-  else if(comando === "diadasmaes"){
-    await message.channel.send("> Porque eu deveria comprar algo? É Dia das Mães, e não das Esposas!")
-  }
-  else if(comando === "perdao"){
-    await message.channel.send("> Sabe quantas vezes eu estava certo e tive que pedir perdão? 4.351 vezes!")
-  }
-  else if(comando === "relaxar"){
-    await message.channel.send("> Por que eu vou sair pra relaxar se eu posso relaxar em casa que é grátis?")
-  }
+
 });
 
 client.login(process.env.TOKEN);

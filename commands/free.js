5f773c5c71c24a19878e0f8dd09dad7b353f5064
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-26 20:26:26 -0300

    embed images

--- commands/free.js
+++ commands/free.js
@@ -13,20 +13,31 @@ module.exports.run = async (client, message, args) => {
             someFreeGamesComing = data.data.Catalog.searchStore.elements;
         });
 
-    let freeGames = "> **Free Games:**\n";
     let startDate = 'undefined'
+    let endDate = 'undefined'
     for(let i=0; i<someFreeGamesComing.length; i++){
         if(someFreeGamesComing[i].promotions.promotionalOffers.length!==0){
             startDate = new Date(someFreeGamesComing[i].promotions.promotionalOffers[0].promotionalOffers[0].startDate)
+            endDate = new Date(someFreeGamesComing[i].promotions.promotionalOffers[0].promotionalOffers[0].endDate)
         }else if(someFreeGamesComing[i].promotions.upcomingPromotionalOffers.length!==0){
             startDate = new Date(someFreeGamesComing[i].promotions.upcomingPromotionalOffers[0].promotionalOffers[0].startDate)
+            endDate = new Date(someFreeGamesComing[i].promotions.upcomingPromotionalOffers[0].promotionalOffers[0].startDate)
         }
-        freeGames += "> **"+someFreeGamesComing[i].title+"** - Date: "+startDate+"\n";
-    }
-    //TODO: show game thumb images / format date / set emojis to free games and non free games yet
 
-    if(freeGames==="> **Free Games:**\n"){
-        freeGames+="> There aren't free games now.\n";
+        const embedFreeGame = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(someFreeGamesComing[i].title)
+            .setURL('https://www.epicgames.com/store/pt-BR/product/'+someFreeGamesComing[i].productSlug)
+            .setAuthor('Epic Julius', 'https://cdn.pixabay.com/photo/2020/05/08/10/10/symbol-5145011_960_720.png')
+            .setDescription(someFreeGamesComing[i].description)
+            .setThumbnail(encodeURI(someFreeGamesComing[i].keyImages[0].url))
+            .addFields(
+                { name: 'Start Date', value: startDate, inline: true },
+                { name: 'End Date', value: endDate, inline: true },
+            )
+            .setImage(encodeURI(someFreeGamesComing[i].keyImages[1].url))
+            .setTimestamp()
+            .setFooter('Game Arrived/Arrive in Epic Games: '+ someFreeGamesComing[i].effectiveDate);
+        await message.channel.send(embedFreeGame);
     }
-    await message.channel.send(freeGames);
 };

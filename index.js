680e95094df496583ac8262c92d94a43075b672c
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2021-02-18 20:59:57 -0300

    guild member add message changed

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.on("ready", () => {
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
   if (!channel) return;
-  channel.send("Bem vindo, "+member,". Entrar no servidor vai lhe custar 1 dólar.");
+  channel.send("Welcome, "+member,". It will cost you a dollar.");
 });
 
 //When someone send a message
